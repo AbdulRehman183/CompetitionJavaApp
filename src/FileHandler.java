74030badf9c5538aacd0846d90a4e14372e2a64a
Abdul Rehman <abdulrehmansed@gmail.com>
2023-12-22 13:00:07 +0500

    file handler class added

---
+++ src/FileHandler.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+import java.util.Date;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+public class FileHandler {
+
+    public List<Competitor> readCompetitorsFromFile(String fileName) throws FileNotFoundException {
+        List<Competitor> competitors = new ArrayList<>();
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+
+            int competitorNumber = Integer.parseInt(parts[0]);
+            String competitorName = parts[1];
+            int age = Integer.parseInt(parts[2]);
+
+            String level = parts[3];
+            String country = parts[4];
+
+            int[] scores = new int[4];
+            for (int i = 0; i < 4; i++) {
+                scores[i] = Integer.parseInt(parts[i + 5]);
+            }
+
+            // System.out.println(competitorNumber + " " + competitorName + " " + age + " "
+            // + level + " " + country + " "
+            // + scores[0] + " " + scores[1] + " " + scores[2] + " " + scores[3]);
+
+            String firstName = competitorName.split(" ")[0];
+            String lastName = competitorName.split(" ")[1];
+
+            LudoCompetitor competitor = new LudoCompetitor(competitorNumber, new Name(firstName, lastName), country,
+                    level, age, scores);
+
+            competitors.add(competitor);
+        }
+
+        scanner.close();
+        return competitors;
+    }
+
+    public void writeCompetitorToFile(Competitor competitor, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            // Writing data in the specified format
+            writer.write(competitor.getCompetitorNumber() + ",");
+            writer.write(competitor.getName() + ",");
+            writer.write(competitor.getLastName() + ","); // Assuming you have a getter for last name
+            writer.write(competitor.getAge() + ",");
+            writer.write(competitor.getGender() + ",");
+            writer.write(competitor.getCountry() + ",");
+
+            // Writing the scores
+            int[] scores = competitor.getScoreArray(); // Assuming you have a getter for the scores
+            for (int score : scores) {
+                writer.write(score + ",");
+            }
+
+            // Move to the next line
+            writer.newLine();
+
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception appropriately in your application
+        }
+    }
+
+}
---
+++ src/Report.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+import java.util.List;
+
+public class Report {
+    private int competitionID;
+    private Date competitionDate;
+    private List<Competitor> registeredCompetitors;
+
+    public Report() {
+    }
+
+    public int getCompetitionID() {
+        return competitionID;
+    }
+
+    public void setCompetitionID(int competitionID) {
+        this.competitionID = competitionID;
+    }
+
+    public Date getCompetitionDate() {
+        return competitionDate;
+    }
+
+    public void setCompetitionDate(Date competitionDate) {
+        this.competitionDate = competitionDate;
+    }
+
+    public List<Competitor> getRegisteredCompetitors() {
+        return registeredCompetitors;
+    }
+
+    public void setRegisteredCompetitors(List<Competitor> registeredCompetitors) {
+        this.registeredCompetitors = registeredCompetitors;
+    }
+}
